e4d185b2ac80ee92a02058f85dfcabd74ff41fc8
Aleksandra Kuznetsova <kuznesashka@gmail.com>
2018-04-05 16:43:52 +0300

    Add correlation with matching filters for 10 spikes

--- Spikes on sensors/SpikeDetect.m
+++ Spikes on sensors/SpikeDetect.m
@@ -5,26 +5,34 @@ function [spike_ind_red] = SpikeDetect(Data, f_low, f_high, check)
 % FORMAT:
 %   SpikeDetect(Data)
 % INPUTS:
-%   Data -- Nch x Ntimes raw data 
+%   Data -- Nch x Ntimes raw data
+%   f_low -- cutoff frequency for the passband filter
+%   f_high -- cutoff frequency for the passband filter
+%   check -- if 1 then draw graphs
 %
 % OUTPUT:
-% 
+%   spike_ind_red -- index of the sample with spike peaks
+%
+% NOTES:
+%   You need to mark manually 10 spikes in your data 
 % _______________________________________________________
 % Aleksandra Kuznetsova, kuznesashka@gmail.com
 % Alexei Ossadtchi, ossadtchi@gmail.com
 
 Fs = 1/Data.Time(2)-Data.Time(1);
+% filter out low frequences!
 [b,a] = butter(3,[f_low f_high]/(Fs/2)); 
 Ff = filtfilt(b,a,Data.F(32:99,:)')';
 
-% 40 independent components
-ncomp = 40;
+% all independent components
+ncomp = 68;
 [w,s] = runica(Ff, 'pca', ncomp);
 W = w*s;
 Q = pinv(W);
 ica_ts = W*Ff;
 
 % 2.1. Kurtosis (how outlier-prone the distribution is)
+% we are looking for a high kurtosis
 kurt = kurtosis(ica_ts');
 [val_kurt, ind_kurt] = sort(kurt, 'descend');
 
@@ -42,13 +50,17 @@ end
 pos_ind = ind_kurt(1:20);
 
 % 2.2. Spectral decomposition of components
+% power index shows the ratio of the slow oscillations
+% should be low
+% attention: it is also low for the heartbeats
+
 for i = 1:length(pos_ind)
     S = fft(ica_ts(pos_ind(i),:));
     f = linspace(0, 250, length(S));
     Pyy = S.*conj(S)/length(S);
     
-    power_low = sum(Pyy(f<=5));
-    power_high = sum(Pyy((f>5)&(f<=125)));
+    power_low = sum(Pyy(f<=10));
+    power_high = sum(Pyy((f>10)&(f<=125)));
     power_index(i) = (power_low/sum(f<=5))/(power_high/sum((f>5)&(f<=125)));
 end
 
@@ -58,26 +70,33 @@ end
 % figure
 % stem(log(power_index))
 
-ind_power= find(log(power_index)<(mean(log(power_index))-std(log(power_index))));
+%ind_power= find(log(power_index)<(mean(log(power_index))));
+ind_power= find(power_index<(mean(power_index)));
+
 
 % 3. Match filter
-x = Ff(:,spikes(10)); % ideal spike
-x = x/norm(x);
-X_n = sqrt(sum(Ff.^2,1)); % norm of the each time sample
-X = bsxfun(@rdivide, Ff, X_n);
-match_filter = x'*X;
-match_filter(match_filter<0.8) = 0;
-
+% for the first 10 spikes
 ica_pos = ica_ts(pos_ind,:);
 ica_pos_n = sqrt(sum(ica_pos.^2,2));
 ica_pos = bsxfun(@rdivide, ica_pos, ica_pos_n);
-match_filter = match_filter/norm(match_filter);
-match_corr = sum(ica_pos.*match_filter,2);
+
+for i = 1:10
+    x = Ff(:,spikes(i)); % ideal spike
+    x = x/norm(x);
+    X_n = sqrt(sum(Ff.^2,1)); % norm of the each time sample
+    X = bsxfun(@rdivide, Ff, X_n);
+    match_filter = x'*X;
+    match_filter(match_filter<0.8) = 0;
+    match_filter = match_filter/norm(match_filter);
+    match_corr(i,:) = abs(sum(ica_pos.*match_filter,2));
+end
 
 % figure
-% stem(match_corr)
+% imagesc(match_corr)
+% colorbar
 
-ind_match= find(match_corr>(mean(match_corr)+std(match_corr)));
+ind_match = find(max(match_corr)>mean(max(match_corr)));
+
 
 % Final decision
 ind_spikecomp = intersect(ind_match, ind_power);
@@ -99,7 +118,7 @@ spike_ind_red = spike_ind(diff>20);
 % size(spike_ind_red)
 
 if check == 1
-    print('Number of picked components ', num2str(ind_spikecomp))
+    disp(['Number of picked components ', num2str(ind_spikecomp)])
     num_val = Fs*10;
     time = 1/Fs:1/Fs:(size(ica_ts,2)/Fs);
     spikes = int32(Data.Events(1).times*Fs+1);
