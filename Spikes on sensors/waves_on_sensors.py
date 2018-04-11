7f4d2cb4d0302e5bbcc451d8a60d21a3bd859e8b
Aleksandra Kuznetsova <kuznesashka@gmail.com>
2018-04-11 11:16:15 +0300

    MNE-python for ICA artefacts deleting

--- Spikes on sensors/waves_on_sensors.py
+++ Spikes on sensors/waves_on_sensors.py
@@ -1,3 +1,27 @@
 import mne
-Data = mne.io.read_raw_ctf('/home/sasha/Data/Wave_prior/Data/MEG/Jordy/jordy_spont03.ds')
+from mne.preprocessing import ICA
 
+Data = mne.io.read_raw_ctf('/home/sasha/Data/Wave_prior/Data/MEG/Kaco/Kaco/kaco_spont01.ds')
+
+Data.load_data()
+
+Data.info
+
+picks = mne.pick_types(Data.info, meg='mag', eeg=False, eog=False,
+                       stim=False, ref_meg = False, exclude='bads')
+Data.filter(1, 40., fir_design='firwin', picks = picks)
+
+n_components = 68
+method = 'fastica'  
+decim = 3  
+random_state = 23
+ica = ICA(n_components=n_components, method=method, random_state=random_state)
+ica.fit(Data, picks=picks, decim=decim)
+
+ica.plot_components(picks=range(40), inst=Data)
+
+ica.plot_sources(Data)
+
+ica.apply(Data, exclude = [0,3,12,19])
+
+Data.save('/home/sasha/Data/Wave_prior/Data/MEG/Kaco/Kaco/kaco_spont01_py.fif')
